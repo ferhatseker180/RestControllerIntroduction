051fb30a9bdcca10f7acb79714ab7508acb7edc9
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-05-03 20:35:27 +0300

    Validation yapısı eklendi ve mail için kullanıldı

--- src/main/java/org/ferhat/restcontroller/entity/Customer.java
+++ src/main/java/org/ferhat/restcontroller/entity/Customer.java
@@ -1,6 +1,7 @@
 package org.ferhat.restcontroller.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ public class Customer {
     @Column(name = "customer_name", nullable = false, length = 50)
     private String name;
 
+    @Email(message = "Invalid mail address")
     @Column(name = "customer_mail", unique = true)
     private String mail;
 
