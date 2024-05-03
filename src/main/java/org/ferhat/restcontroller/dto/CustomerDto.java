4e944cb15c2408b1954331f386611e5416476e6c
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-05-03 20:36:05 +0300

    Validation yapısı eklendi ve mail için kullanıldı

--- src/main/java/org/ferhat/restcontroller/dto/CustomerDto.java
+++ src/main/java/org/ferhat/restcontroller/dto/CustomerDto.java
@@ -1,5 +1,7 @@
 package org.ferhat.restcontroller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,9 @@ import lombok.NoArgsConstructor;
 public class CustomerDto {
 
     private int id;
+    @NotNull
     private String name;
+    @Email
     private String mail;
 
 }
